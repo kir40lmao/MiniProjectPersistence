9cd002e10724dfa1afa23ad54dd060efc1dd811a
KornelKutrovics <93515112+KornelKutrovics@users.noreply.github.com>
2022-03-24 10:10:00 +0100

    Update DBConnection.java

--- src/dataaccess/DBConnection.java
+++ src/dataaccess/DBConnection.java
@@ -1,6 +1,30 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-	//New comment for DavidP / test
-	//adbah
+
+	final static String connectionString = "jdbc:sqlserver://hildur.ucn.dk\\SQLExpress;databaseName=CSC-CSD-S212_10407565;user=CSC-CSD-S212_10407565;password=Password1!;encrypt=false;";
+	private static DBConnection contextInstance;
+	private Connection dbConnection;
+	private DBConnection() throws Exception {
+		try {
+		dbConnection = DriverManager.getConnection(connectionString);
+	} catch (SQLException e) {
+		throw new Exception("Error connecting to atabase. Error was: '" + e.getMessage() + "'", e);
+	}
+		}
+	public static DBConnection getInstance() throws Exception {
+		
+			if(contextInstance == null) {
+				contextInstance = new DBConnection();
+			}
+		
+		return contextInstance;
+	}
+	
+	public Connection getConnection() {		return dbConnection;	}
 }
+
