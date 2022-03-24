48d877d1e99862678b025070239d0b99231e35b7
paripasd <paripasd@gmail.com>
2022-03-24 10:49:26 +0100

    wefer

--- src/dataaccess/CustomerDAOIn.java
+++ src/dataaccess/CustomerDAOIn.java
@@ -1,5 +1,12 @@
 package dataaccess;
 
+import ModelLayer.Customer;
+
 public interface CustomerDAOIn {
+	void createCustomer();
+	Customer readCustomer();
+	Customer readCustomerByPhoneNumber(int phoneNumber);
+	void updateCustomer();
+	void deleteCustomer();
 
 }
--- src/dataaccess/ItemDAOIn.java
+++ src/dataaccess/ItemDAOIn.java
@@ -1,5 +1,11 @@
 package dataaccess;
 
-public interface ItemDAOIn {
+import ModelLayer.Item;
 
+public interface ItemDAOIn {
+	void createItem();
+	Item readItem();
+	Item readItemById(int id);
+	void updateItem();
+	void deleteItem();
 }
